8dddefb227cbb2b401b9a8d6d7e0c9e695e81763
Keyur Mistry <keyurmistry334@gmail.com>
2026-07-21 11:24:15 +0530

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -66,7 +66,8 @@ export function hexToHsl(hex: string): string {
   const b = parseInt(hex.substring(4, 6), 16) / 255
 
   const max = Math.max(r, g, b), min = Math.min(r, g, b)
-  let h = 0, s = 0, l = (max + min) / 2
+  const l = (max + min) / 2
+  let h = 0, s = 0
 
   if (max !== min) {
     const d = max - min
