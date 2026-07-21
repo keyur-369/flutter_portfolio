96f8000f294c61fbf8cbb271620c385b683e8c8a
Keyur Mistry <keyurmistry334@gmail.com>
2026-07-21 13:07:23 +0530

    fix 3d

--- components/home/Hero3D.tsx
+++ components/home/Hero3D.tsx
@@ -1,4 +1,3 @@
-/// <reference types="@react-three/fiber" />
 'use client'
 
 import { useRef } from 'react'
---
+++ r3f.d.ts
@@ -0,0 +1,3 @@
+// Makes React Three Fiber JSX intrinsic elements (<group>, <mesh>, <ambientLight>, etc.)
+// available globally without needing a triple-slash reference in every file.
+import '@react-three/fiber'
